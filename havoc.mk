27719eebece3e6f68b96321052eecfb426f978b8
BobbyDev21 <bobby.dev2021@gmail.com>
2022-04-14 17:35:44 +0000

    Initial Upload

---
+++ havoc.mk
@@ -0,0 +1,22 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy Tab A
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 768
+TARGET_SCREEN_HEIGHT := 1024
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
