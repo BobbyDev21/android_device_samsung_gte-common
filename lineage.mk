32a2422560f4aed600e23fd5e6557f3f637a45d3
BobbyDev21 <bobby.dev2021@gmail.com>
2022-04-18 23:39:50 +0000

    Initial Upload

---
+++ lineage.mk
@@ -0,0 +1,19 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy Tab A
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 768
+TARGET_SCREEN_HEIGHT := 1024
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
